2ffbcd0cc6314d527f383075ac636429c14942bd
Walter He <64016753+WalterHe0808@users.noreply.github.com>
2024-05-21 17:41:44 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -13,14 +13,14 @@ const BLOG = {
   APPEARANCE_DARK_TIME: process.env.NEXT_PUBLIC_APPEARANCE_DARK_TIME || [18, 6], // 夜间模式起至时间，false时关闭根据时间自动切换夜间模式
 
   // 3.14.1版本后，欢迎语在此配置，英文逗号隔开 ,  即可支持多个欢迎语打字效果。
-  GREETING_WORDS: process.env.NEXT_PUBLIC_GREETING_WORDS || 'Hi，这是一个人工智能的学习频道，分享：AI以及前沿科技的干货内容,欢迎来到AI时代🎉',
+  GREETING_WORDS: process.env.NEXT_PUBLIC_GREETING_WORDS || '这里是清华哥AI工作流系统，欢迎来到AI流量获客时代🎉',
 
   CUSTOM_MENU: process.env.NEXT_PUBLIC_CUSTOM_MENU || false, // 支持Menu 类型，从3.12.0版本起，各主题将逐步支持灵活的二级菜单配置，替代了原来的Page类型，此配置是试验功能、默认关闭。
 
   AUTHOR: process.env.NEXT_PUBLIC_AUTHOR || '清华哥', // 您的昵称 例如 tangly1024
   BIO: process.env.NEXT_PUBLIC_BIO || '知识顶流MCN创始人', // 作者简介
   LINK: process.env.NEXT_PUBLIC_LINK || 'https://jiehe.tech', // 网站地址
-  KEYWORDS: process.env.NEXT_PUBLIC_KEYWORD || 'AI知识库, 博客', // 网站关键词 英文逗号隔开
+  KEYWORDS: process.env.NEXT_PUBLIC_KEYWORD || '清华哥, AI获客', // 网站关键词 英文逗号隔开
 
   // 社交链接，不需要可留空白，例如 CONTACT_WEIBO:''
   CONTACT_EMAIL: process.env.NEXT_PUBLIC_CONTACT_EMAIL || 'jiehe_01@163.com', // 邮箱地址 例如mail@tangly1024.com
